2c55d35f9a0c469756067d116199817608b25ced
Álex Román Núñez <eirexe123@gmail.com>
2025-10-26 20:32:40 +0100

    Try to save some disk space.

--- misc/build_profiles/production_android.py
+++ misc/build_profiles/production_android.py
@@ -2,7 +2,7 @@ import os
 
 custom_modules = "../modules"
 platform = "android"
-ffmpeg_path = os.path.abspath("../buildeps/ffmpeg-android-maker/build/ffmpeg/arm64-v8a")
+ffmpeg_path = os.path.abspath("../buildeps/ffmpeg-android")
 target = "template_release"
 dev_build = "no"
 generate_apk = "yes"
